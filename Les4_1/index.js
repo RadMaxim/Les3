fac17b143a3b9dbf2f1a6073ba822850701f5d9c
Maksim <radchuk.maksime@gmail.com>
2024-09-29 08:40:38 +0300

    les 5

---
+++ Les4_1/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	let isShown = true;
+	$(".panel").click(function () {
+		if (isShown) {
+			$(".notification").hide();
+			
+		} else {
+			$(".notification").show();
+			
+		}
+        isShown = !isShown;
+	});
+});
